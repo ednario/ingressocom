a94193981e221aeeeb9d166cb23e378a4a320dd5
Ednario Andrade <ednariocoder@gmail.com>
2023-11-28 16:10:56 -0300

    fix: ensure carousel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useMediaQuery } from 'react-responsive';
 
 import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/solid'
 
@@ -15,25 +16,37 @@ function Carousel({ slides, type }) {
     else setCurrent(current + 1);
   };
 
+  const isDesktopOrLaptop = useMediaQuery({
+    query: '(min-width: 1280px)'
+  });
+
   return (
-    <div className="bg-gray-800 relative">
+    <div className="bg-gray-800">
       <h1 className="pt-12 pb-6 pl-4 text-white text-lg font-bold">{type}</h1>
       <div
         className={`flex transition ease-out duration-40 ml-6 py-1 gap-3 relative group xl:w-[14.12rem] xl:h-[20.93rem] xs:w-[8.12rem] xs:h-[12.12rem]`}
-        style={{
-          transform: `translateX(-${current * 100}%)`,
-        }}
+        style={
+          isDesktopOrLaptop ? 
+          {
+            width: "150%",
+            transform: `translateX(-${current * 5}%)`,
+          } : 
+          {
+            width: "300%",
+            transform: `translateX(-${current * 5}%)`,
+          }
+        }
       >
         {slides.map((s) => (
-              <a key={s.id} className="" href="">
+              <a key={s.id} className="" href={s.link}>
                 <img
-                src={s.link}
+                src={s.img}
                 alt="img"
                 className="rounded-2xl" />
                 </a>
         ))}
         </div>
-        <div className="absolute top-[15px] h-full w-full justify-between items-center flex text-white px-10 text-3xl xs:top-12">
+        <div className="relative h-full w-full justify-between items-center flex text-white px-10 text-3xl xl:top-[-12rem] xs:top-[-6rem]">
             <button onClick={previousSlide}>
                   <ChevronLeftIcon className="text-blue-400 h-8 w-8" />
                 </button>
